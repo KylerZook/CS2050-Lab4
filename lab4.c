4accff5d2216c4193d228077af81b0159301fad6
ZayKay47u <kylerzook@gmail.com>
2025-02-24 23:51:42 -0600

    Added comments

--- lab4.c
+++ lab4.c
@@ -3,92 +3,94 @@
 
 void ** allocateJagged2DArray(int elementSize, int rows, int lengths[]){
     void** array = malloc(sizeof(int) + rows * sizeof(void*));  
-    if(!array) return NULL;  // Early exit if allocation fails.
+    if(!array) return NULL;  //early exit if allocation fails.
 
-    *((int*)array) = rows;  // Store number of rows.
-    void** base = (void**)((int*)array + 1);  // Move base pointer to avoid overwriting row count.
+    *((int*)array) = rows;  //store number of rows.
+    void** base = (void**)((int*)array + 1);  //move base pointer to avoid overwriting row count.
 
     for(int i = 0; i < rows; i++){
-        base[i] = malloc(sizeof(int) + lengths[i] * elementSize);
-        *(int*)base[i] = lengths[i];  // Store column count at beginning
-        base[i] = (void*)((int*)base[i] + 1);  // Move pointer forward so users only see data.
+        base[i] = malloc(sizeof(int) + lengths[i] * elementSize); //allocates room for lengths[i] many elements plus an int
+        *(int*)base[i] = lengths[i];  //store column count at beginning
+        base[i] = (void*)((int*)base[i] + 1);  //move pointer forward so users only see data
     }
 
-    return base; 
+    return base; //returns array
 }
 
 
 void freeJagged2DArray(void ** arrayJagged){
-    if(!arrayJagged) return;
+    if(!arrayJagged) return; //checks if valid array
     
-    int rows = *((int*)arrayJagged - 1);
+    int rows = *((int*)arrayJagged - 1); //gets rows
     for(int i = 0; i < rows; i++){
-        void * original_columns = (void*)((int*)arrayJagged[i] - 1);
-        free(original_columns);
+        void * original_columns = (void*)((int*)arrayJagged[i] - 1); //moves back pointer in columns
+        free(original_columns); //frees columns
     }
-    void** original_row = (void**)((int*)arrayJagged - 1);
-    free(original_row);
+    void** original_row = (void**)((int*)arrayJagged - 1); //moves back pointer in row
+    free(original_row);//frees row
 }
 
 int getRowCount(void ** arrayJagged){
-    if(!arrayJagged){ return 0; }
-    return *((int*)arrayJagged - 1);
+    if(!arrayJagged){ return 0; } //checks if valid array
+    return *((int*)arrayJagged - 1); //returns real first element
 }
 int getColCount(void ** arrayJagged, int row){
-    if(!arrayJagged || row < 0){ return 0; }
+    if(!arrayJagged || row < 0){ return 0; } //checks if valid array and if row count is more than 0
     
-    return *(((int*)arrayJagged[row]) - 1);
+    return *(((int*)arrayJagged[row]) - 1); //returns first element of said row
 }
 
 
 Course * createCourse(int number, char * department, char * name){
-    if(number < 0 || !department || !name){ return NULL; }
+    if(number < 0 || !department || !name){ return NULL; } //if all valid paremeters
 
-    Course * new_course = malloc(sizeof(Course));
-    if(!new_course){ return NULL; }
-
+    Course * new_course = malloc(sizeof(Course)); //allocates space for new course
+    if(!new_course){ return NULL; } //checks if allocated
+    //sets values to given in parameters
     new_course->number = number;
     strcpy(new_course->name, name);
     strcpy(new_course->department, department);
-    return new_course;
+    return new_course; //returns course pointer
 }
 
-// destructor
+
 void freeCourse(Course * pCourse){
-    if(!pCourse){ return; }
-    free(pCourse);
+    if(!pCourse){ return; } //parameter check
+    free(pCourse); //frees course pointer
 }
 
 // getters - return 1 for success or 0 for failure
 int getCourseInfo(Course * pCourse, char * outputString){
-    if(!pCourse || !outputString){ return 0;}
-    size_t size = 50;
+    if(!pCourse || !outputString){ return 0;} //checks if valid parameters
+    size_t size = 50; //max string size because i got a warning or sum
+
+    //takes department, name, and number from course pointer and combines into string of size 50
     snprintf(outputString, size,"%s (%s) - %d", pCourse->name, pCourse->department, pCourse->number);
     return 1;
 }
 
 // setters - return 1 for success or 0 for failure
 int setCourseNumber(Course * pCourse, int number){
-    if(!pCourse || number < 0){ return 0; }
+    if(!pCourse || number < 0){ return 0; } //parameter check
 
-    pCourse->number = number;
+    pCourse->number = number; //sets course number to given
     return 1;
 }
 int setCourseDepartment(Course * pCourse, char * department){
-    if(!pCourse || !department){ return 0; }
+    if(!pCourse || !department){ return 0; }//parameter check
 
-    strcpy(pCourse->department, department);
+    strcpy(pCourse->department, department);//copys given to course department
     return 1;
 }
 int setCourseName(Course * pCourse, char * name){
-    if(!pCourse || !name){ return 0; }
+    if(!pCourse || !name){ return 0; }//parameter check
 
-    strcpy(pCourse->name, name);
+    strcpy(pCourse->name, name);//copys given to course name
     return 1;
 }
 
-int return_rand_index(int min, int max){
-    return(rand()% (max - min + 1) + min);
+int return_rand_index(int min, int max){ 
+    return(rand()% (max - min + 1) + min);//returns random number between min and max index inclusive
 }
 
 
--- main.c
+++ main.c
@@ -1,23 +1,31 @@
 #include "lab4.h"
 int main(void) {
+    //given values for testing
     srand(time(NULL));
     int rows = 3;
     int lengths[3] = {4, 5, 6};
     int course_numbers[] = {1050, 2050, 3050};
     char *names[] = {"Kyler", "John", "Zook", "Job", "Daniel", "Wargo"};
     char *department[] = {"CS", "Math", "English", "Engineering", "Chemistry"};
-    char info[50];
-    void **mem_block = allocateJagged2DArray((int)sizeof(Course), rows, lengths);
-    Course **courses = (Course **)mem_block;
+    char info[50]; //empty string to print course info
+    
+    
+    void **mem_block = allocateJagged2DArray((int)sizeof(Course), rows, lengths); //init memory block
+    Course **courses = (Course **)mem_block; //changes to course ** type
 
     for (int i = 0; i < getRowCount((void **)courses); i++) {
-        for (int j = 0; j < getColCount((void **)courses, i); j++) {
+        for (int j = 0; j < getColCount((void **)courses, i); j++) { //for each element of array
+            
+            //creates course pointer in new
             Course * new =  createCourse(course_numbers[return_rand_index(0, 2)],department[return_rand_index(0, 4)], names[return_rand_index(0, 5)]);
-            setCourseDepartment(&courses[i][j], new->department);
+            
+            //sets current address in array to new courses values
+            setCourseDepartment(&courses[i][j], new->department); 
             setCourseName(&courses[i][j], new->name);
             setCourseNumber(&courses[i][j], new->number);
-            if (getCourseInfo(&courses[i][j], info) == 1) {
-                printf("%s\n", info);
+            
+            if (getCourseInfo(&courses[i][j], info) == 1) { //if getcourse info was successful
+                printf("%s\n", info); //prints each course
             } else {
                 continue;
             }
@@ -25,9 +33,9 @@ int main(void) {
     }
 
     for (int i = 0; i < rows; i++) {
-        printf("column count for row %d: %d\n", i, getColCount((void **)courses, i));
+        printf("column count for row %d: %d\n", i, getColCount((void **)courses, i)); //getcolumn check for no reason ig
     }
-    printf("rows: %d\n", getRowCount((void **)courses));
+    printf("rows: %d\n", getRowCount((void **)courses)); //getrow check for no reason
 
     // Free the entire jagged array
     freeJagged2DArray((void **)courses);
